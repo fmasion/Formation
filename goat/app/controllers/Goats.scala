e1c29345bc3f7cd5967915fe5842f8d9830e2271
Karol Chmist <karolchmist@gmail.com>
2015-11-18 21:08:10 +0100

    Two goats in sequence

--- goat/app/controllers/Goats.scala
+++ goat/app/controllers/Goats.scala
@@ -3,11 +3,10 @@ package controllers
 import java.util.UUID
 
 import models.Goat
-import play.api.Play
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.{WSResponse, WS}
 import play.api.mvc._
-import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.Play.current
 
 import scala.concurrent.Future
@@ -23,12 +22,17 @@ class Goats extends Controller {
       case None => Future.successful(BadRequest("me prend pas pour un con c'est pas une chèvre"))
       case Some(g) => {
         val goatWithId = g.id.map{id => g}.getOrElse(g.copy(id = Some(UUID.randomUUID().toString) ))
+        val goat2 = goatWithId.copy(name = goatWithId.name + "_2")
 
         val result: Future[WSResponse] = WS.url("http://localhost:9200/animals/goat").post(Json.toJson(goatWithId))
-        result.map{ wsresponse =>
-          Ok(wsresponse.body)
-        }.recover{
-          case e => BadRequest
+        val result2: Future[WSResponse] = WS.url("http://localhost:9200/animals/goat").post(Json.toJson(goat2))
+
+        val eventualResponses: Future[Seq[WSResponse]] = Future.sequence(Seq(result, result2))
+
+        eventualResponses.map{ (wsresponses: Seq[WSResponse]) =>
+          val bodies = wsresponses.map(_.body)
+          val reponseString = bodies.mkString(", ")
+          Ok(reponseString)
         }
       }
     }
@@ -36,5 +40,3 @@ class Goats extends Controller {
   }
 
 }
-
-
