47a25a1daf9f0d55dd68ff867502aba30d7167ee
Frederic Masion <f.masion@kreactive.com>
2015-11-25 15:00:11 +0100

    send goats in space yoohooooo

--- goat/app/controllers/Goats.scala
+++ goat/app/controllers/Goats.scala
@@ -4,16 +4,12 @@ import java.util.UUID
 
 import models.Goat
 import play.api.libs.json.{JsValue, Json}
-import play.api.libs.ws.{WSResponse, WS}
 import play.api.mvc._
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import play.api.Play.current
 
 import scala.concurrent.Future
 
-/**
- * Created by fred on 04/11/2015.
- */
+
 class Goats extends Controller {
 
   def save = Action.async(parse.json){ (r: Request[JsValue]) =>
@@ -22,21 +18,35 @@ class Goats extends Controller {
       case None => Future.successful(BadRequest("me prend pas pour un con c'est pas une chèvre"))
       case Some(g) => {
         val goatWithId = g.id.map{id => g}.getOrElse(g.copy(id = Some(UUID.randomUUID().toString) ))
-        val goat2 = goatWithId.copy(name = goatWithId.name + "_2")
 
-        val result: Future[WSResponse] = WS.url("http://localhost:9200/animals/goat").post(Json.toJson(goatWithId))
-        val result2: Future[WSResponse] = WS.url("http://localhost:9200/animals/goat").post(Json.toJson(goat2))
+        val clones: List[Goat] = (1 to 10).toList.map(clone(goatWithId, _))
 
-        val eventualResponses: Future[Seq[WSResponse]] = Future.sequence(Seq(result, result2))
+        val goatList =  goatWithId :: clones
+//
+//        val content = goatList match{
+//          case Nil => "empty"
+//          case x::Nil => "just One"
+//          case x::Goat("blanchette", _, _)::Nil  => "just Two and first name : " + x.name
+//          case x::xs if goatList.size ==43 => "many"
+//          case _ => "unhandled"
+//        }
 
-        eventualResponses.map{ (wsresponses: Seq[WSResponse]) =>
-          val bodies = wsresponses.map(_.body)
-          val reponseString = bodies.mkString(", ")
-          Ok(reponseString)
+        val savedGoats: List[Future[(String,Boolean)]] = goatList.map(_.save)  // Goat => Future[WsReesponse]
+
+        val eventualResponses: Future[List[(String,Boolean)]] = Future.sequence(savedGoats)
+
+        eventualResponses.map{ (wsresponses: List[(String,Boolean)]) =>
+         if(wsresponses.forall(b => b._2)){
+           Ok(Json.toJson(wsresponses.toMap))
+         }else{
+           BadRequest(Json.toJson(wsresponses.toMap))
+         }
         }
       }
     }
 
   }
+
+  private def clone(goat:Goat, index:Int) =  goat.copy(name = goat.name + s"_$index", id = Some(UUID.randomUUID().toString))
 
 }
--- goat/app/models/Goat.scala
+++ goat/app/models/Goat.scala
@@ -1,14 +1,42 @@
 package models
 
+import play.api.Logger
 import play.api.libs.json.Json
+import play.api.libs.ws.{WSResponse, WS}
 
-/**
- * Created by fred on 21/10/2015.
- */
-case class Goat(name:String, color:String, id:Option[String]=None)
+import scala.util.{Failure, Success}
+
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import play.api.Play.current
+
+import scala.concurrent.Future
+
+
+case class Goat(name:String, color:String, id:Option[String]=None){
+  def save = Goat.save(this)
+
+}
 
 object Goat{
+  val log = Logger(getClass)
   //implicit val goatFormat= Json.format[Goat]
   implicit val goatReads= Json.reads[Goat]
   implicit val goatWrites= Json.writes[Goat]
+
+  def save(goat:Goat): Future[(String,Boolean)] = {
+    // Assert id is set
+    val id = goat.id.getOrElse(throw new IllegalArgumentException("Missing ID"))
+
+    // Save
+    val respF = WS.url("http://localhost:9200/animals/goat").post(Json.toJson(goat))
+    respF.map { (resp: WSResponse) =>
+      log.error("Response " + resp)
+      resp.status match {
+        case i if i>=200 & i < 300 => id -> true
+        case _ => log.error("Response " + resp) ; id -> false
+      }
+    }.recover {
+      case e => id -> false
+    }
+  }
 }
